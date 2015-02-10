1c918524972d3ffda369700ee2ae85da04112f5c
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-02-11 01:19:01 +1000

    Cam works. Mouse works. Instantiate on world works.

---
+++ Regrowth Unity/Assets/Scripts/GameController.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class GameController : MonoBehaviour {
+
+	RaycastHit mouseClick;
+	public GameObject tree;
+
+
+	void Start () {
+	
+	}
+
+	void Update () {
+
+		//Left click - seed pick up / placement
+		if (Input.GetMouseButtonDown (0)) {
+			Ray ray = Camera.main.ScreenPointToRay (Input.mousePosition);
+			if (Physics.Raycast(ray, out mouseClick, 1000)){
+				//if seeds > 0 instantiate seedling
+				if (mouseClick.collider.tag == "Ground"){
+					Instantiate (tree, mouseClick.point, Quaternion.identity);
+				}
+			}
+		}
+
+	}
+}
---
+++ Regrowth Unity/Assets/Scripts/InstRotate.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class InstRotate : MonoBehaviour {
+
+	public Transform centerobj;
+	
+	void Start () {
+		transform.LookAt (centerobj);
+	}
+
+}
