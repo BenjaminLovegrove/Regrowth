ae1040f3cb245d8f885c8a09985a94943762e020
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-02-11 00:33:13 +1000

    Import assets. Create framework. Camera setup.

---
+++ Regrowth Unity/Assets/Scripts/CameraManager.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class CameraManager : MonoBehaviour {
+
+	public Transform centerobj;
+	public float camspeed = 75f;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+
+		//Rotate Camera Around World
+		transform.LookAt (centerobj);
+
+		if (Input.GetKey (KeyCode.W) && transform.rotation.x < 85) {
+			transform.Translate(Vector3.up * camspeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.A)) {
+			transform.Translate(Vector3.left * camspeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.S)) {
+			transform.Translate(Vector3.down * camspeed * Time.deltaTime);
+		}
+		if (Input.GetKey (KeyCode.D)) {
+			transform.Translate(Vector3.right * camspeed * Time.deltaTime);
+		}
+
+		transform.LookAt (centerobj);
+
+	}
+
+
+}
