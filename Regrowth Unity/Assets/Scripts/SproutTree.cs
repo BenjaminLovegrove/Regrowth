b78bf6257559524a7d7567cabe7e649eb0f057fa
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-02-12 11:11:54 +1000

    Trees light on fire. Die after x seconds.

--- Regrowth Unity/Assets/Scripts/SproutTree.cs
+++ Regrowth Unity/Assets/Scripts/SproutTree.cs
@@ -10,14 +10,30 @@ public class SproutTree : MonoBehaviour {
 	bool grown = false;
 	int danger = 0;
 	float lifetimer = 8;
+	bool burning = false;
+
+	Transform fireGraph1;
+	Transform fireGraph2;
+	Transform fireGraph3;
+	Transform fireGraph4;
+	Transform fireGraph5;
+	int fireRNG;
 
 	void Start () {
 		treeGraph = transform.Find ("Graphics");
 		sproutGraph = transform.Find ("Sprout");
+		fireGraph1 = transform.Find ("FlamingTree/Fire");
+		fireGraph2 = transform.Find ("FlamingTree/Tree");
+		fireGraph3 = transform.Find ("FlamingTree/Fire.001");
+		fireGraph4 = transform.Find ("FlamingTree/Fire.002");
+		fireGraph5 = transform.Find ("FlamingTree/Fire.003");
+
+
 		growTimer = Random.Range (25, 50);
 	}
 	
 	void Update () {
+		//Make tree grow up
 		growTimer -= Time.deltaTime;
 
 		if (growTimer <= 0 && grown == false) {
@@ -25,12 +41,21 @@ public class SproutTree : MonoBehaviour {
 			grown = true;
 		}
 
+		//When tree is grown
 		if (grown == true) {
 			//Do tree stuff
+
+			//Burst into flames
+			fireRNG = Random.Range (0,10000);
+			if (fireRNG < 500 && burning == false){
+				OnFire();
+			}
+
+			//If tree is dying from vines
 			if (danger < 0){
 				danger = 0;
 			}
-
+			// If vines nearby lower life time, else reset life time
 			if (danger > 0){
 				lifetimer -= Time.deltaTime;
 			} else {
@@ -43,6 +68,18 @@ public class SproutTree : MonoBehaviour {
 				Camera.main.SendMessage("RemoveTree");
 			}
 		}
+	}
+
+	void OnFire(){
+		danger ++;
+		burning = true;
+		treeGraph.renderer.enabled = false;
+		fireGraph1.renderer.enabled = true;
+		fireGraph2.renderer.enabled = true;
+		fireGraph3.renderer.enabled = true;
+		fireGraph4.renderer.enabled = true;
+		fireGraph5.renderer.enabled = true;
+
 	}
 
 
