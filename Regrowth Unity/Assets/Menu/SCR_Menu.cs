2ba4af8cf35767ab288a68066de5a66a65a60ebd
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-02-13 22:16:19 +1000

    Added menu

---
+++ Regrowth Unity/Assets/Menu/SCR_Menu.cs
@@ -0,0 +1,30 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SCR_Menu : MonoBehaviour {
+
+	public Texture menuHeader;
+	public Transform centerobj;
+
+	void Update () {
+
+		transform.LookAt (centerobj);
+		transform.Translate(Vector3.right * 1 * Time.deltaTime);
+		transform.LookAt (centerobj);
+
+
+	}
+
+	void OnGUI(){
+
+		GUI.DrawTexture (new Rect (Screen.width * 0.5f, 0, 512, 256), menuHeader);
+
+		if (GUI.Button(new Rect(Screen.width * 0.7f, Screen.height * 0.45f, 200, 50), "Play")){
+			Application.LoadLevel ("Homeworld");
+		}
+		if (GUI.Button(new Rect(Screen.width * 0.7f, Screen.height * 0.555f, 200, 50), "Quit")){
+			Application.Quit();
+		}
+
+	}
+}
