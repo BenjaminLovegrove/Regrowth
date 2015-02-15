57d2110eb21cce403c04ea7cfcad82390f3b1369
BenjaminLovegrove <Benjamin_jl@Live.com>
2015-02-15 10:32:48 +1000

    Sync to laptop

---
+++ Regrowth Unity/Assets/Scripts/PlanetCollider.cs
@@ -0,0 +1,22 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class PlanetCollider : MonoBehaviour {
+
+	bool touchdown = false;
+
+	void Update(){
+		if (!touchdown) {
+			SendMessageUpwards("Bump", SendMessageOptions.DontRequireReceiver);
+		}
+	}
+
+	void OnCollisionEnter (Collision col) {
+		print ("fgt");
+		if (col.gameObject.tag == "Ground") {
+
+			touchdown = true;
+		}
+
+	}
+}
